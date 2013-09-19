162ca1b3ec44500c6e673fa458c8a35b0b8aa391
Luca Bellenghi <lucabel@gmail.com>
2013-09-19 09:52:06 +0200

    string conversion of sent and received folder title: unicode it's not a good idea for uid catalog

--- Products/IMS/content/message_folder.py
+++ Products/IMS/content/message_folder.py
@@ -4,18 +4,19 @@
 from zope.interface import implements
 from zope.i18n import translate
 
-from Products.CMFCore.permissions import ModifyPortalContent, DeleteObjects
-
 from Products.Archetypes.public import *
 
 from Products.ATContentTypes.content.base import ATCTMixin
 
 from Products.IMS.config import PROJECTNAME
-from Products.IMS.interfaces import IMessageFolder, IReceivedMessageFolder, ISentMessageFolder
+from Products.IMS.interfaces import (IMessageFolder,
+                                     IReceivedMessageFolder,
+                                     ISentMessageFolder)
 from Products.IMS import IMSMessageFactory as _
 
 MessageFolderSchema = BaseFolderMixin.schema.copy()
 MessageFolderSchema.delField('title')
+
 
 class BaseMessageFolder(BaseFolderMixin, ATCTMixin):
     """ A folder holding messages
@@ -24,33 +25,38 @@ class BaseMessageFolder(BaseFolderMixin, ATCTMixin):
 
     _at_rename_after_creation = False
     schema = BaseFolderMixin.schema.copy()
-    
+
+
 class ReceivedMessageFolder(BaseMessageFolder):
     """ The folder holding received messages
     """
     implements(IReceivedMessageFolder)
-    
+
     portal_type = meta_type = "ReceivedMessageFolder"
-    
+
     def Title(self):
         try:
-            return translate(_('title_receivedmessagefolder', default=u'Received Messages'), context=self.REQUEST)
+            return translate(_('title_receivedmessagefolder',
+                             default=u'Received Messages'),
+                             context=self.REQUEST).encode('utf-8')
         except:
-            return u'Received Messages'
-    
+            return u'Received Messages'.encode('utf-8')
+
+
 class SentMessageFolder(BaseMessageFolder):
     """ The folder holding sent messages
     """
     implements(ISentMessageFolder)
-    
+
     portal_type = meta_type = "SentMessageFolder"
-    
+
     def Title(self):
         try:
-            return translate(_('title_sentmessagefolder', default=u'Sent Messages'), context=self.REQUEST)
+            return translate(_('title_sentmessagefolder',
+                             default=u'Sent Messages'),
+                             context=self.REQUEST).encode('utf-8')
         except:
-            return u'Sent Messages'
-    
+            return u'Sent Messages'.encode('utf-8')
+
 registerType(ReceivedMessageFolder, PROJECTNAME)
 registerType(SentMessageFolder, PROJECTNAME)
-    
